ae558fb7d478813fdabfd4a350850ccd6b64e02c
itamarit <itamarit124812@gmail.com>
2022-12-29 19:09:44 +0200

    add board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,36 +1,51 @@
-import {useState,useEffect} from "react";
-import {useParams} from "react-router-dom";
-import {getBoard,createTask} from "../rest"
+import { useState, useEffect } from "react";
+import { Form, useParams } from "react-router-dom";
+import { getBoard, createTask } from "../rest"
 
- export const  Board =(props) =>{
+export const Board = (props) => {
 
-const [tasks, setTasks] = useState([]);
-const [users, setUsers] =useState([]);
-const [statuses ,setStatuses ] = useState([]);
-const {boardId} = useParams();
+  const [tasks, setTasks] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [statuses, setStatuses] = useState([]);
+  const [TaskTypes, setTaskTypes] = useState([]);
+  const { boardId } = useParams();
 
 
-    useEffect(() => {
-        async function init() {
-        let board = await getBoard(boardId); 
-        console.log(board);
-        setTasks(board.tasks);
-        } 
-        init();
+  useEffect(() => {
+    async function init() {
+      let board = await getBoard(boardId);
+      console.log(board);
+      setTasks(board.tasks);
+      setUsers(board.users)
+      setStatuses(board.statuses)
+      setTaskTypes(board.taskTypes)
+    }
+    init();
+  }, []);
 
-       
-    }, []);
-
-useEffect(() => {
+  useEffect(() => {
     console.log(tasks);
-    }, [tasks]);
+  }, [tasks]);
 
-   return (
-                <div>
-                <h1>Hello</h1>
-                <button onClick={() =>createTask()}>add new task</button>
-                <button>filter</button>
-                </div>
-    )
+  return (
+    <div>
+      <ul>{tasks && tasks.map(
+        (task, index) => <li className="message-li" key={index}>
+          <div className="task">
+            <form className="taskForm">
+              <h3 className="taskTitle">{task.title}</h3>
+              <p className="taskDescription">{task.description}</p>
+              <div className="taskDetails">
+                <p className="taskDueDate">Due date: {task.dueDate}</p>
+                <p className="taskStatus">Status: {task.status}</p>
+              </div>
+            </form>
+          </div>
+        </li>
+      )}</ul>
+      <button onClick={() => createTask()}>add new task</button>
+      <button>filter</button>
+    </div>
+  )
 }
 
